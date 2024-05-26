a78089204ec9ca52bb67275b30f54467bd6b14d5
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2024-05-26 08:54:41 +0530

    dash board design

--- Dashboard.js
+++ Dashboard.js
@@ -1,42 +1,37 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  Switch,
-  Pressable,
-  Image,
-} from "react-native";
-
-export default function Dashboard({navigation}) {
+import { StyleSheet, Text, View, Button,Image } from "react-native";
+import {groceries} from './assets/groceries.png';
+import {homeware}  from './assets/homeware.png';
+export default function Dashboard({ navigation }) {
   return (
-    <View >
-       <Button
-    title='Login'
-    onPress={() => navigation.navigate('Login')}/>
+    <View>
+      <View style={style.navMenu}>
+        <Button title="Login" onPress={() => navigation.navigate("Login")} />
+      </View>
       <View style={style.ctgSection}>
         <View style={style.flexCol}>
           <View style={style.ctg}>
             <Text>Groceries</Text>
+            <Image source={require('./assets/groceries.png')} style={style.ctgImg}></Image>
           </View>
           <View style={style.ctg}>
-          <Text>Groceries</Text>
+            <Text>Home ware</Text>
+            <Image source={require('./assets/homeware.png')} style={style.ctgImg}></Image>
           </View>
           <View style={style.ctg}>
-          <Text>Clothings</Text>
+            <Text>Clothings</Text>
+            <Image source={require('./assets/cloths.png')} style={style.ctgImg}></Image>
           </View>
           <View style={style.ctg}>
-          <Text>Electronics</Text>
+            <Text>Electronics</Text>
+            <Image source={require('./assets/electronics.png')} style={style.ctgImg}></Image>
           </View>
           <View style={style.ctg}>
-          <Text>Accessories</Text>
+            <Text>Vegetables</Text>
+            <Image source={require('./assets/vegetables.png')} style={style.ctgImg}></Image>
           </View>
-          <View style={style.ctg}>
-          <Text>Others</Text>
-          </View>
+
         </View>
       </View>
 
@@ -50,6 +45,7 @@ const style = StyleSheet.create({
     backgroundColor: "#fff",
     padding: 20,
     justifyContent: "center",
+
   },
   header: {
     position: "absolute",
@@ -63,22 +59,33 @@ const style = StyleSheet.create({
   },
   logo: { width: 150, height: 50, marginTop: 5, marginBottom: 5 },
   loginContainer: {
-    marginRight:10,
+    marginRight: 10,
   },
-  ctgSection:{
-   justifyContent:'center'
+  navMenu: {
+    marginRight: 5,
+    marginLeft: "auto",
   },
-  flexCol:{
-    display:'flex',
-    flexDirection:'row',
-    flexWrap:'wrap',
-    justifyContent1:'space-between'
+  ctgSection: {
+    flex:1,
+    justifyContent:"space-evenly",
+      },
+  ctgImg:{
+    width: 100,
+    height:100,
+    marginTop: 5, 
+  marginBottom:5 
   },
-  ctg:{
-    width:'30%',
-    borderColor:'black',
-    borderWidth:2,
-    padding:10
-
-  }
+  flexCol: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  ctg: {
+    width: "30%",
+    borderColor: "black",
+    borderWidth: 2,
+    padding: 10,
+    margin:10
+  },
 });
--- Login.js
+++ Login.js
@@ -1,129 +1,157 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Switch, Pressable,Image } from 'react-native';
-export default function Login({navigation}) {
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Switch,
+  Pressable,
+  Alert,
+} from "react-native";
+
+export default function Login({ navigation }) {
   const [click, setClick] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
     // Add login logic here
     if (username.length > 0 && password.length > 0) {
-      console.log('Username:', username);
-      console.log('Password:', password);
+      console.log("Username:", username);
+      console.log("Password:", password);
+    } else {
+      console.log("Empty inputs");
     }
-    else {
-      console.log('empty inputs')
-    }
-
   };
 
   return (
-    <View style={styles.loginContainer}>
-       <Button
-    title='Dashboard'
-    onPress={() => navigation.navigate('Dashboard')}/>
-      <View >
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Username</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Enter username"
-          onChangeText={setUsername}
-          value={username}
+    <View style={styles.screenContainer}>
+      <View style={styles.navMenu}>
+        <Button
+          title="&#8962;"
+          onPress={() => navigation.navigate("Dashboard")}
+          style={styles.navBtn}
         />
       </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Enter password"
-          onChangeText={setPassword}
-          value={password}
-          secureTextEntry
-        />
-      </View>
-      <View style={styles.rememberView}>
-        <View style={styles.switch}>
-          <Switch value={click} onValueChange={setClick} trackColor={{ true: "green", false: "gray" }} />
-          <Text style={styles.rememberText}>Remember Me</Text>
+      <View style={styles.loginContainer}>
+        <View style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Username</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter username"
+              onChangeText={setUsername}
+              value={username}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Password</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter password"
+              onChangeText={setPassword}
+              value={password}
+              secureTextEntry
+            />
+          </View>
+          <View style={styles.rememberView}>
+            <View style={styles.switchContainer}>
+              <Switch
+                value={click}
+                onValueChange={setClick}
+                trackColor={{ true: "green", false: "gray" }}
+              />
+              <Text style={styles.rememberText}>Remember Me</Text>
+            </View>
+            <Pressable onPress={() => Alert.alert("Forget Password!")}>
+              <Text style={styles.forgetText}>Forgot Password?</Text>
+            </Pressable>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Login"
+              onPress={handleLogin}
+              style={styles.submitButton}
+            />
+          </View>
         </View>
-        <View>
-          <Pressable onPress={() => Alert.alert("Forget Password!")}>
-            <Text style={styles.forgetText}>Forgot Password?</Text>
-          </Pressable>
+        <View style={styles.signUpContainer}>
+          <Text style={styles.footerText}>
+            Don't Have Account?
+            <Button title="Sign up" style={styles.submitButton} />
+          </Text>
         </View>
+        <StatusBar style="auto" />
       </View>
-      <View style={styles.signup}>
-      <Button title="Login" onPress={handleLogin} style={styles.submitButton}  />
-      </View>
-      </View>
-      <View style={styles.signup}>
-      <Text style={styles.footerText}>Don't Have Account?  <Button title='Sign up' style={styles.submitButton} /></Text>
-       </View>
-      <StatusBar style="auto" />
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  loginContainer:{
-    display:'flex',
-    width:'100%',
-    marginHorizontal:5
+  screenContainer:{
+    flex: 1,
   },
+  navMenu:{
 
-  submitButton:{
-    height : 45,
-    width:48,
-    borderWidth  : 1,
-    borderRadius : 5,
-    alignItems : "center",
-    justifyContent : "center",
-    marginHorizontal:15
+    marginRight:5,
+  marginLeft:'auto'
+  },
+  loginContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  formContainer: {
+    width: "100%",
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 15,
-    width: '100%',
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 15,
   },
   label: {
-    flex: '1',
-    textAlign: 'right',
-    marginRight: 5,
+    width: 100,
     fontSize: 16,
-    paddingHorizontal: 10,
   },
   textInput: {
-    flex: '2',
+    flex: 1,
     height: 40,
-    borderColor: 'black',
+    borderColor: "gray",
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
   },
-  rememberText: {
-    fontSize: 13
-  },
-  forgetText: {
-    fontSize: 11,
-    color: "red"
-  },
   rememberView: {
-    width: "100%",
-    paddingHorizontal: 50,
+    flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 15,
+  },
+  switchContainer: {
     flexDirection: "row",
-    marginBottom: 8
+    alignItems: "center",
   },
-  signup:{
-    margin:'5px',
+  rememberText: {
+    marginLeft: 10,
+    fontSize: 16,
   },
-  footerText:{
-    marginRight:'5px'
-  }
-
+  forgetText: {
+    color: "red",
+    fontSize: 16,
+  },
+  buttonContainer: {
+    marginVertical: 10,
+  },
+  signUpContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  footerText: {
+    fontSize: 16,
+  },
+  submitButton: {
+    marginLeft: 5,
+  },
 });
